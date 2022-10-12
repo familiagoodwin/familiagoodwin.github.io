b044626ab43ee0b29b61a24c7a2becf4c9dc5ef1
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-12 20:59:26 -0300

    add main menu basico

--- fmlgoodwin/src/App.js
+++ fmlgoodwin/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Capa from './pages/Capa';
+import MainMenu from './components/MainMenu';
 
 function App() {
   return (
     <div className="App">
      <Capa />
+     <MainMenu />
     </div>
   );
 }
---
+++ fmlgoodwin/src/components/MainMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MainMenu() {
+  return (
+    <div>
+      <ul>
+        <li>Albuns</li>
+        <li>teste 01</li>
+        <li>teste 02</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MainMenu
