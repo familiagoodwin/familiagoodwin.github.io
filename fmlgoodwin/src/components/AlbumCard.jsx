cb2ac33772676b6b18b3c5c257b1ef6754180435
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-22 15:17:00 -0300

    att comp AlbumCard: add handleClick

--- fmlgoodwin/src/components/AlbumCard.jsx
+++ fmlgoodwin/src/components/AlbumCard.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import GlobalVarsContext from '../contexts/context/GlobalVars';
 
 function AlbumCard() {
+  const { setClassCss } = useContext(GlobalVarsContext);
+
+  function handleClick() {
+    setClassCss((prev) => ({...prev, mainCape: `noBody`}));;
+  }
+
   return (
-    <div className='cardAlbum'>
+    <div className='cardAlbum' onClick={ handleClick }>
       <h1 className='titleCardAlbum'>Album 0001</h1>
     </div>
   )
