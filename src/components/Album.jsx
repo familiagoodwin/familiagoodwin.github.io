91e4cd9e34acf497e32efe5fb0ea725730bdf9a9
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-23 22:54:01 -0300

    virando até a pagina 03

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from 'react'
 import guia from '../services/guiaFotos'
 
 function Album({ index }) {
-  const [ pagina ] = useState(0);
+  const [ pagina, setPagina ] = useState(0);
+  const [ flip, setFlip ] = useState({ num1: true, num2: false });
+  const album = guia[index - 1];
+
+  console.log(flip);
+  console.log(pagina);
 
   useEffect(()=> {
     const btnNext = document.querySelector('.fa-circle-right');
@@ -16,16 +21,78 @@ function Album({ index }) {
     }
   },[]);
 
+  const navNext = (actual) => {
+    const pagLeft = document.querySelector('.num1');
+    const pagRigth = document.querySelector('.num2');
+
+    console.log(actual - 1 % 2);
+    if (actual - 1 % 2 === 0) {
+      pagLeft.style.zIndex = "1"; 
+      pagRigth.style.zIndex = "0";
+      setPagina((prev) => prev + 1);
+      setFlip((prev) => ({ ...prev, num1: false}));
+      pagRigth.classList.remove('pageNext');
+      pagLeft.classList.add('pageNext');
+      setFlip((prev) => ({ ...prev, num2: true }));
+    } else {
+      pagLeft.style.zIndex = "0"; 
+      pagRigth.style.zIndex = "1";
+      setPagina((prev) => prev + 1);
+      setFlip((prev) => ({ ...prev, num2: false }));
+      pagLeft.classList.remove('pageNext');
+      pagRigth.classList.add('pageNext');
+      setFlip((prev) => ({ ...prev, num1: true }));
+    }
+
+    if (actual < album.paginas.length - 1) {
+      console.log('teste');
+
+    }
+  }
+
+  function navClick({ target }) {
+    // console.log(album.paginas.length);
+    if (target.id === 'navNext' && pagina + 1 < album.paginas.length - 1) {
+      navNext(pagina + 1);
+    }
+  }
+
   return (
     <section className='corpoAbum'>
-      <div className='page num1'>
-        <img className='imgFoto' src={guia[index - 1].paginas[pagina].url} alt={ `Pagina ${pagina}` } />
-      </div>
-      <div className='page num2'>
-        <img className='imgFoto' src={guia[index - 1].paginas[pagina + 1].url} alt={ `Pagina ${pagina + 1}` } />
-      </div>
-      <i class="fa-regular fa-circle-left fa-4x iconeNav" />
-      <i class="fa-regular fa-circle-right fa-4x iconeNav"></i>
+      {/* <div className='page num1'>
+        <img className='imgFoto' src={album.paginas[pagina].url} alt={ `Pagina ${pagina}` } />
+      </div> */}
+      { (flip.num1) ? (
+          <div className='page num1'>
+            <img className='imgFoto' src={album.paginas[pagina].url} alt={ `PaginaA ${pagina}` } />
+          </div>
+        ) : (
+          <div className='page num1'>
+            <img className='imgFoto' src={album.paginas[pagina + 1].url} alt={ `PaginaA ${pagina + 1}` } />
+          </div>
+        )
+      }
+      { (flip.num2) ? (
+          <div className='page num2'>
+            <img className='imgFoto' src={album.paginas[pagina].url} alt={ `PaginaB ${pagina}` } />
+          </div>
+        ) : (
+          <div className='page num2'>
+            <img className='imgFoto' src={album.paginas[pagina + 1].url} alt={ `PaginaB ${pagina + 1}` } />
+          </div>
+        )
+      }
+      
+      <i 
+        id='navPrev'
+        onClick={ navClick }
+        class="fa-regular fa-circle-left fa-4x iconeNav" 
+      />
+      <i 
+        id='navNext'
+        onClick={ navClick }
+        class="fa-regular fa-circle-right fa-4x iconeNav" 
+      />
     </section>
   )
 }
--- src/services/guiaFotos.js
+++ src/services/guiaFotos.js
@@ -18,10 +18,22 @@ const guia = [
       {
         url: 'https://docs.google.com/uc?id=1fuafRUtR8ko_Vd4Mrpuu-QDlgGu1fGMz',
         fotos: ['0001']
+      },
+      {
+        url: 'https://docs.google.com/uc?id=133TuYqEDuvZYPL2Ma3rAi-hnln6y6ZSB',
+        fotos: ['0001']
+      },
+      {
+        url: 'https://docs.google.com/uc?id=1uYC3z_j2Ez1LhgEY2mVk6Bse5pCMEFuk',
+        fotos: ['0001']
+      },
+      {
+        url: '',
+        fotos: ['0001']
       }
     ]
   },
 ];
 
 export default guia;
-//https://drive.google.com/file/d/1fuafRUtR8ko_Vd4Mrpuu-QDlgGu1fGMz/view?usp=sharing
+//https://drive.google.com/file/d/1uYC3z_j2Ez1LhgEY2mVk6Bse5pCMEFuk/view?usp=sharing
